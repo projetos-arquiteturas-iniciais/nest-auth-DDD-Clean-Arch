1c04e4343ef0d94d85f40c30e00492c86cf726bb
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-02-24 16:30:49 -0300

    feat: Email unit tests - validation

--- src/shared/domain/velue-objects/email.spec.ts
+++ src/shared/domain/velue-objects/email.spec.ts
@@ -1,9 +1,25 @@
 import { Email } from './email';
+import { BadRequestError } from '@shared/domain/errors';
 
 describe('Email unit tests', () => {
   it('shoud create an email', () => {
     const email = new Email('email@example.com');
 
     expect(email.value).toStrictEqual('email@example.com');
+  });
+
+  it('should throw a BadRequestError email is invalid', () => {
+    expect(() => new Email('@dsf.com')).toThrow(
+      new BadRequestError(`email in invalid format`),
+    );
+    expect(() => new Email('ssdwd.com')).toThrow(
+      new BadRequestError(`email in invalid format`),
+    );
+    expect(() => new Email('swddw@')).toThrow(
+      new BadRequestError(`email in invalid format`),
+    );
+    expect(() => new Email('dsa@defew')).toThrow(
+      new BadRequestError(`email in invalid format`),
+    );
   });
 });
--- src/shared/domain/velue-objects/email.ts
+++ src/shared/domain/velue-objects/email.ts
@@ -1,11 +1,23 @@
+import { BadRequestError } from '../errors';
+import { Validator } from '../validations';
+
 export class Email {
   private _email: string;
 
   constructor(email: string) {
     this._email = email;
+    this.validation();
   }
 
   get value(): string {
     return this._email;
+  }
+
+  private validation(): void {
+    const isValid = Validator.isEmail(this._email);
+
+    if (!isValid) {
+      throw new BadRequestError('email in invalid format');
+    }
   }
 }
