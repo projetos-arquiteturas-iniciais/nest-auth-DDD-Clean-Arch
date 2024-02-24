32cf29d46f51d19325908362d8d8a16ece691b7b
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-02-24 16:13:59 -0300

    feat: validations creation

---
+++ src/shared/domain/errors/index.ts
@@ -0,0 +1,8 @@
+export * from './conflict-error';
+export * from './not-found-error';
+export * from './invalid-credentials-error';
+export * from './invalid-password-error';
+export * from './bad-request-error';
+export * from './unprocessable-entity-error';
+export * from './internal-server-error';
+// export * from './';
--- src/shared/domain/validations/index.ts
+++ src/shared/domain/validations/index.ts
@@ -1,2 +1,12 @@
+export * from './validation.composite';
+export * from './validation.inteface';
 export * from './validator';
+export * from './required-field.validation';
+export * from './email.validation';
+export * from './uuid.validation';
+export * from './max-length-field.validation';
+export * from './max-value-field.validation';
+export * from './min-length-field.validation';
+export * from './min-value-field.validation';
+export * from './strong-password.validation';
 // export * from './'
