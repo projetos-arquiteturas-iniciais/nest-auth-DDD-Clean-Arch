011b744d532bdf41d5f0eb294ce8beb76ce85bfc
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-02-28 18:22:53 -0300

    feat: JsonWebTokenAdapter creation

---
+++ src/shared/infra/jwt/jesonwebtoken/jesonwebtoken.adapter.spec.ts
@@ -0,0 +1,45 @@
+import { UnauthorizedError } from '@shared/domain/errors';
+import { JsonWebTokenAdapter } from './jesonwebtoken.adapter';
+
+describe('JsonWebTokenAdapter integration tests', () => {
+  const secretKey = 'mySecretKey';
+  const jwtAdapter = new JsonWebTokenAdapter(secretKey);
+
+  it('should create and decode a JWT token correctly', () => {
+    const payload = { userId: 12345 };
+    const token = jwtAdapter.sign(payload, { expiresIn: '1h' });
+    const decodedPayload = jwtAdapter.decode(token);
+
+    expect(decodedPayload.userId).toEqual(12345);
+    expect(decodedPayload.exp).toBeDefined();
+    expect(decodedPayload.iat).toBeDefined();
+  });
+
+  it('should throw an UnauthorizedError if the JWT token is expired', async () => {
+    const payload = { userId: 12345 };
+
+    const token = jwtAdapter.sign(payload, { expiresIn: '1s' });
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    expect(() => jwtAdapter.verify<{ userId: number }>(token)).toThrow(
+      new UnauthorizedError('Token JWT expired'),
+    );
+  });
+
+  it('should throw an UnauthorizedError if the JWT token is not valid', async () => {
+    expect(() => jwtAdapter.verify('invalid token')).toThrow(
+      new UnauthorizedError('Action not allowed'),
+    );
+  });
+
+  it('should not accept JWT token in valid format but with another secret key', async () => {
+    const anotherSecretKey = 'anotherSecretKey';
+    const another = new JsonWebTokenAdapter(anotherSecretKey);
+    const payload = { userId: 12345 };
+    const token = another.sign(payload, { expiresIn: '1h' });
+
+    expect(() => jwtAdapter.verify<{ userId: number }>(token)).toThrow(
+      new UnauthorizedError('Action not allowed'),
+    );
+  });
+});
