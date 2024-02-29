5e4bc89f6406767ed7a8e5f130aba9164e1e730a
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-02-29 19:15:47 -0300

    feat: AuthUsecasesFactory - signin method implemantation

---
+++ src/modules/auth/application/usecases/auth.usecases.factory.ts
@@ -0,0 +1,15 @@
+import { Signin } from '@auth/application/usecases';
+import { DefaultUseCase } from '@shared/domain/usecases';
+import { IUserRepository } from '@users/domain/repositories';
+import { IHasher } from '@shared/domain/crypto';
+import { userRepositoryFactory } from '@users/infra/data/repositories';
+import { hasherFactory } from '@shared/infra/crypto/hasher';
+
+export class AuthUsecasesFactory {
+  public static signin(): DefaultUseCase {
+    const repo: IUserRepository = userRepositoryFactory();
+    const hasher: IHasher = hasherFactory();
+
+    return new Signin.UseCase(repo, hasher);
+  }
+}
