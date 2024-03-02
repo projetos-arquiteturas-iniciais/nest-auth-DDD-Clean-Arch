42848b1269770d506e6e5f83c498f6ffbc941859
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-03-02 14:19:39 -0300

    feat: use OnlyAdminGuard

--- src/modules/auth/infra/main/auth.controller.ts
+++ src/modules/auth/infra/main/auth.controller.ts
@@ -9,7 +9,7 @@ import {
 } from '@nestjs/common';
 
 import { IsPublic } from '@shared/infra/decorators';
-import { LocalAuthGuard } from '@auth/infra/main/guards';
+import { LocalAuthGuard, OnlyAdminGuard } from '@auth/infra/main/guards';
 import { AuthRequest, CreateUserDTO } from '@auth/infra/main/dtos';
 import { UserUseCasesFactory } from '@users/application/usecases';
 import { AuthUseCasesFactory } from '@auth/application/usecases';
@@ -18,6 +18,7 @@ import { AuthUseCasesFactory } from '@auth/application/usecases';
 export class AuthController {
   @Post('sign-up')
   @HttpCode(HttpStatus.CREATED)
+  @UseGuards(OnlyAdminGuard)
   public signup(@Body() body: CreateUserDTO) {
     const usecase = UserUseCasesFactory.createUser();
 
