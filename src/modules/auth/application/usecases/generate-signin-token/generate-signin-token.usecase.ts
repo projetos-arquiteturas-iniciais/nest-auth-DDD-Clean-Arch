ffa6a96586fc58d778e474b47368a499b4b0cb0c
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-03-02 11:32:13 -0300

    feat: GenerateSigninToken.UseCase - implemantation

---
+++ src/modules/auth/application/usecases/generate-signin-token/generate-signin-token.usecase.ts
@@ -0,0 +1,36 @@
+import { requestUser } from '@auth/infra/main/dtos';
+import { DefaultUseCase } from '@shared/domain/usecases';
+import { IJsonWebToken } from '@shared/infra/jwt';
+
+export namespace GenerateSigninToken {
+  export type UserPayload = {
+    sub: string;
+    email: string;
+    name: string;
+    iat?: number;
+    exp?: number;
+  };
+
+  export type Input = requestUser;
+
+  export type Output = {
+    access_token: string;
+  };
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private readonly JWTProvider: IJsonWebToken) {}
+
+    public async execute(input: Input): Promise<Output> {
+      const payload: UserPayload = {
+        sub: input['id'],
+        name: input['name'],
+        email: input['email'],
+      };
+      const jwtToken = this.JWTProvider.sign(payload, { expiresIn: '1d' });
+
+      return {
+        access_token: jwtToken,
+      };
+    }
+  }
+}
